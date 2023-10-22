6091f250aa24bd9bdcbe7059b7dac595ca4f0ba2
Pierre Epron <pierre.epron@gmail.com>
2023-10-22 05:42:05 +0200

    improve clm tokenizer

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -3,10 +3,9 @@ def cls_tokenize(tokenizer, parent_text, text, **kwargs):
     return tokenizer(f"{parent_text}{tokenizer.eos_token}{tokenizer.bos_token}{text}", **kwargs)
 
 def clm_tokenize(tokenizer, prompt, system_prompt, instruct_prompt, item, **kwargs):
+    instruct_prompt = instruct_prompt.format(parent_text=item['parent_text'], text=item['text'])
     tokens = prompt.format(
         system_prompt=system_prompt, 
         instruct_prompt=instruct_prompt, 
-        parent_text=item['parent_text'], 
-        text=item['text']
     )
     return tokenizer.encode(tokens, **kwargs)
