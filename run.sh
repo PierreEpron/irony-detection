04f8aecdf6819b157b19fb5788201c5017db6c21
Pierre Epron <pierre.epron@gmail.com>
2023-12-03 15:19:53 +0100

    change hf cache directory

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
+export HF_HOME="/srv/storage/idmctal@storage1.nancy.grid5000.fr/2023/m2/pepron/.cache/huggingface"
+export HF_DATASETS_CACHE="/srv/storage/idmctal@storage1.nancy.grid5000.fr/2023/m2/pepron/.cache/huggingface/datasets"
+export TRANSFORMERS_CACHE="/srv/storage/idmctal@storage1.nancy.grid5000.fr/2023/m2/pepron/.cache/huggingface/models"
+
 cd ${HOME}/irony-detection/
 nvcc --version
 source venv/bin/activate
 python3 -m src.experiments.$1
-
