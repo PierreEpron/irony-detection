6b6304373ad018bcc60bb92b72e5f56bfdbf4f89
Pierre Epron <pierre.epron@gmail.com>
2023-12-05 17:54:20 +0100

    Implement multiple loss training

---
+++ src/training.py
@@ -0,0 +1,50 @@
+from transformers import Trainer
+from torch import nn
+import torch
+
+class MCC_Loss(nn.Module):
+    """
+    From: https://github.com/kakumarabhishek/MCC-Loss
+    Calculates the proposed Matthews Correlation Coefficient-based loss.
+
+    Args:
+        inputs (torch.Tensor): 1-hot encoded predictions
+        targets (torch.Tensor): 1-hot encoded ground truth
+    """
+
+    def __init__(self):
+        super(MCC_Loss, self).__init__()
+
+    def forward(self, inputs, targets):
+        """
+        MCC = (TP.TN - FP.FN) / sqrt((TP+FP) . (TP+FN) . (TN+FP) . (TN+FN))
+        where TP, TN, FP, and FN are elements in the confusion matrix.
+        """
+        _inputs = inputs[..., 1]
+        tp = (_inputs * targets).sum()
+        tn = ((1 - _inputs) * (1 - targets)).sum()
+        fp = (_inputs * (1 - targets)).sum()
+        fn = ((1 - _inputs) * targets).sum()
+
+        numerator = tp * tn - fp * fn
+        denominator = torch.sqrt((tp+fp) * (tp+fn) * (tn+fp) * (tn+fn))
+
+        # Adding 1 to the denominator to avoid divide-by-zero errors.
+        mcc = numerator / (denominator + 1.0)
+
+        return 1 - mcc
+    
+class IronyTrainer(Trainer):
+    def __init__(self, loss_funcs, **kwargs):
+        super().__init__(**kwargs)
+        # Instantiate loss functions with their custom parameters
+        self.loss_funcs = loss_funcs
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.pop("labels")
+        # Forward
+        outputs = model(**inputs)
+        logits = nn.Softmax(dim=-1)(outputs["logits"])
+        # compue loss and sum their weight 
+        loss = sum([f(logits, labels) * w for f, w in self.loss_funcs])
+        return (loss, outputs) if return_outputs else loss
