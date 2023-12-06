8e1d7568d70402ac29c5f35dc0fb1679f6bdcd14
Pierre Epron <pierre.epron@gmail.com>
2023-12-06 21:05:36 +0100

    renaming

--- src/experiments/clm/epic_random_closed_double_zs.py
+++ src/experiments/clm/epic_random_closed_double_zs.py
@@ -4,7 +4,7 @@ from src.model import clm_run
 if __name__ == "__main__":
     config = load_config()
     config = config | {
-        'RESULT_PATH':"results/epic_random_closed_zs.jsonl",
+        'RESULT_PATH':"results/epic_random_closed_double_zs.jsonl",
         'CLM_PHRASES_PATH':"src/prompts/double_phrases.json"
     }
     clm_run(config)
--- src/experiments/clm/tweeteval_random_closed_single_zs.py
+++ src/experiments/clm/tweeteval_random_closed_single_zs.py
@@ -4,7 +4,7 @@ from src.model import clm_load_tweeteval, clm_run
 if __name__ == "__main__":
     config = load_config()
     config = config | {
-        'RESULT_PATH':"results/tweeteval_random_closed_zs.jsonl",
+        'RESULT_PATH':"results/tweeteval_random_closed_single_zs.jsonl",
         'CLM_PHRASES_PATH':"src/prompts/single_phrases.json"
     }
     clm_run(config, load_data_func=clm_load_tweeteval)
