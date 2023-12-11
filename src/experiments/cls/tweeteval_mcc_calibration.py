439108eed032b7222a3fdf3c4efac560c431287b
Pierre Epron <pierre.epron@gmail.com>
2023-12-11 18:39:10 +0100

    new experiments

---
+++ src/experiments/cls/tweeteval_mcc_calibration.py
@@ -0,0 +1,25 @@
+from src.utils import load_config
+from src.model import cls_run
+from src.model import cls_load_tweeteval
+from src.tokenizer import cls_single_tokenize
+from src.training import MCC_Loss
+
+
+if __name__ == "__main__":
+    config = load_config()
+    config = config | {
+        'CLS_MODEL_NAME':'cardiffnlp/twitter-roberta-large-2022-154m',
+        'LOSS_FUNCS': [
+            (MCC_Loss(), 1), 
+        ],
+    }
+
+    for bs in [16, 32, 48]:
+        for lr in [1e-3, 1e-4, 1e-5]:    
+            path = f"results/tweeteval_{bs}_{lr[-1]}_mcc"
+            config = config | {
+                'OUTPUT_DIR':path, 
+                'RESULT_PATH':f"{path}.jsonl",
+            }
+            cls_run(config, load_data_func=cls_load_tweeteval, tokenize_func=cls_single_tokenize)
+
