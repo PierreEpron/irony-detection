fb8df08e22009f166f7de30bc80089aa7a325cea
Pierre Epron <pierre.epron@gmail.com>
2023-12-22 04:00:25 +0100

    fix

--- extract_results.py
+++ extract_results.py
@@ -13,7 +13,7 @@ for path in results_path.glob(f'{prefix}*'):
     if path.is_dir():
         ts_path = path / 'trainer_state.json'
         cp_path = repo_path / json.loads(ts_path.read_text())['best_model_checkpoint']
-        jsonl_path = path.with_suffix('.jsonl')
+        jsonl_path = Path(str(path) + '.jsonl')
         shutil.copyfile(ts_path, home_path / f'{ts_path.parts[-2]}_state.json')
         shutil.copytree(cp_path, home_path / cp_path.parts[-2])
         shutil.copyfile(jsonl_path, home_path / jsonl_path.parts[-1])
