578e125ab2dea3c0b21196dd2a6a9b40e80e48ff
Pierre Epron <pierre.epron@gmail.com>
2023-12-22 03:53:52 +0100

    rewrite to better handle differents files

--- extract_results.py
+++ extract_results.py
@@ -9,15 +9,12 @@ home_path = Path.home()
 repo_path = home_path / 'irony-detection'
 results_path = repo_path / 'results'
 
-def get_new_path(path):
-    return Path('/'.join(path.parts[:2] + path.parts[3:]))
-
 for path in results_path.glob(f'{prefix}*'):
     if path.is_dir():
         ts_path = path / 'trainer_state.json'
         cp_path = repo_path / json.loads(ts_path.read_text())['best_model_checkpoint']
         jsonl_path = path.with_suffix('.jsonl')
-        shutil.copyfile(ts_path, get_new_path(ts_path))
-        shutil.copytree(cp_path, get_new_path(cp_path))
-        shutil.copyfile(jsonl_path, get_new_path(jsonl_path))
+        shutil.copyfile(ts_path, home_path / f'{ts_path.parts[-2]}_state.json')
+        shutil.copytree(cp_path, home_path / cp_path[-1])
+        shutil.copyfile(jsonl_path, home_path / jsonl_path[-1])
         shutil.rmtree(path)
