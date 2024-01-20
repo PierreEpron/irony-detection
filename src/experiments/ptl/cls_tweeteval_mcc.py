b7475725cc7c95f38a6d92239d3f34ac048587b2
Pierre Epron <pierre.epron@gmail.com>
2024-01-20 18:22:17 +0100

    improve patience

--- src/experiments/ptl/cls_tweeteval_mcc.py
+++ src/experiments/ptl/cls_tweeteval_mcc.py
@@ -124,7 +124,7 @@ trainer = Trainer(
     default_root_dir=RESULT_PATH,
     max_epochs=EPOCHS, 
     log_every_n_steps=50, 
-    callbacks=[EarlyStopping(monitor="val_loss", mode="min")]
+    callbacks=[EarlyStopping(monitor="val_loss", patience=5, mode="min")]
 )
 
 trainer.fit(model=model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
