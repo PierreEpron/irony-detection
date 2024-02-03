bc4238f3851f0027ced50c26fd267633b24b6769
Pierre Epron <pierre.epron@gmail.com>
2024-02-03 23:01:39 +0100

    change some paths

--- src/experiments/ptl/cardiff_epic_bce.py
+++ src/experiments/ptl/cardiff_epic_bce.py
@@ -17,11 +17,11 @@ LEARNING_RATE = 1e-5
 PATIENCE = 5
 MAX_LEN = 125
 MODEL_NAME = "cardiffnlp/twitter-roberta-large-2022-154m"
-RESULT_PATH = Path('results/cardiff_tweeteval_bce')
+RESULT_PATH = Path('results/cardiff_epic_bce')
 
 if not RESULT_PATH.is_dir():
     RESULT_PATH.mkdir()
-    
+
 config = load_config()
 
 def run(k):
