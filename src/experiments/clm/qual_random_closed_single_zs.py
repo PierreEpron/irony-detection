963e8dd9969a8acde8067717efe70ea22c7f17bf
Pierre Epron <pierre.epron@gmail.com>
2024-02-05 16:15:23 +0100

    add more qualitative test

---
+++ src/experiments/clm/qual_random_closed_single_zs.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from src.utils import load_config
+from src.model import clm_load_tweeteval, clm_run
+
+def load_qual():
+    return [{'id_original':i, 'text':l, 'label':0} for i, l in enumerate(Path('data/qualitative.txt').read_text().splitlines())]
+
+if __name__ == "__main__":
+    config = load_config()
+
+    config = config | {
+        'RESULT_PATH':"results/tweeteval_random_closed_single_zs.jsonl",
+        'CLM_PHRASES_PATH':"src/prompts/single_phrases.json"
+    }
+    clm_run(config, load_data_func=load_qual)
