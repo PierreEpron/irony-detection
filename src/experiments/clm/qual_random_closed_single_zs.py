5ead1ab90019dbc5906a5130c984d0024530e07c
Pierre Epron <pierre.epron@gmail.com>
2024-02-05 16:48:30 +0100

    fix

--- src/experiments/clm/qual_random_closed_single_zs.py
+++ src/experiments/clm/qual_random_closed_single_zs.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from src.utils import load_config
 from src.model import clm_load_tweeteval, clm_run
 
-def load_qual():
+def load_qual(config):
     return [{'id_original':i, 'text':l, 'label':0} for i, l in enumerate(Path('data/qualitative.txt').read_text().splitlines())]
 
 if __name__ == "__main__":
